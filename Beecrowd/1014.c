2316fdb757a7d4148aebb87d28f765ab2346a5e2
mdshakibsami <shakib.sami2.0@gmail.com>
2023-03-02 23:20:44 +0600

    km/l problem

---
+++ Beecrowd/1014.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+    int distace;
+    float spentFuel,average;
+    scanf("%d %f",&distace,&spentFuel);
+    average =distace/spentFuel;
+    printf("%.3f km/l\n",average);
+
+}
