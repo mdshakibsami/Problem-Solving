a7039780d132bc596fa98f5362bd8dcfd32d7281
mdshakibsami <shakib.sami2.0@gmail.com>
2023-05-05 21:48:50 +0600

    changed

--- ClassProblem/Lab_problem_8.c
+++ ClassProblem/Lab_problem_8.c
@@ -1,32 +1,16 @@
-/*
-
-Problem 8
-Problem: Write a C Program to show the inverted full pyramid of �*� with N rows as follows.
-Use the following functions to do this.
-
- * * * * * * * * *
- * * * * * * *
- * * * * *
- * * *
- *
-
-Input: Ask user to enter N from console.
-Output
-Output a Pyramid of numbers like of above depending on the user�s input.
-
-
-*/
+;
 
 
 #include<stdio.h>
 int main(){
 
-int n;
+int n,row,col;
 printf("Enter a number\n");
 scanf("%d",&n);
-for(int row=n;row>=1;row=row-2){
+for(row=n;row>0;row--){
 
-    for(int col=1;col<=row;col++){
+
+    for(col=(row*2)-1;col>0;col--){
 
         printf("*");
     }
