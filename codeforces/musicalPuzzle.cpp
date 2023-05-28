6fbc6872990663ff7c0804417881019b241bd2d6
mdshakibsami <shakib.sami2.0@gmail.com>
2023-05-29 03:07:34 +0600

    initial commit

---
+++ codeforces/musicalPuzzle.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int n, same = 0, diff = 1;
+    cin >> n;
+    char arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    for (int i = 1; i < n; i++)
+    {
+        if (arr[0] == arr[i])
+            same = 1;
+        else
+        {
+            same = 0;
+            break;
+        }
+    }
+            
+
+    if(same)
+    {
+        cout<<1;
+    }
+    else
+    {
+        int count=0;
+        for(int i =0;i<n;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(arr[i]==arr[j+1])
+                {
+                     if(arr[i+1]==arr[j+2])
+                        cout<<arr[j]<<endl;
+
+                }
+                   
+               
+            }
+        }
+
+        
+    }
+
+    return 0;
+}
