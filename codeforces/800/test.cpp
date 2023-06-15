40af5cf5f7c4f426fb4f1ec25ddd4228763d42b4
mdshakibsami <shakib.sami2.0@gmail.com>
2023-06-15 10:51:46 +0600

    initial commit

--- codeforces/800/test.cpp
+++ codeforces/800/test.cpp
@@ -1,5 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
+/*you don't need to match exactly testcase if your condition is fulfilled,
+cause sometimes one problem can have multiple answer*/
 
 int main()
 {
@@ -7,45 +9,33 @@ int main()
     cin >> t;
     while (t--)
     {
-        int x, y;
-        string str1, str2;
-        cin >> str1 >> str2;
-        for (int i = 0; i < size(str1); i++)
+        int n;
+        cin >> n;
+        int arr[n];
+        for (int i = n; i >= 1; i--)
         {
-            if (str1[i] == 'M')
-            {
-                x = 2;
-            }
-            else if (str1[i] == 'L')
-            {
-                x = 3 + (size(str1) - 1);
-            }
-            else if (str1[i] == 'S')
-            {
-                x = 1 - (size(str1) - 1);
-            }
+            arr[i] = i;
         }
-        for (int i = 0; i < size(str2); i++)
+        if(n==3)
+            cout<<-1<<endl;
+         else if(n%2==0)
         {
-            if (str2[i] == 'M')
+            
+            for(int i=n;i>=1;i--)
             {
-                y = 2;
+                cout<<arr[i]<<" ";
             }
-            else if (str2[i] == 'L')
-            {
-                y = 3 + (size(str2) - 1);
-            }
-            else if (str2[i] == 'S')
-            {
-                y = 1 - (size(str2) - 1);
-            }
+            cout<<endl;
         }
-        if (x > y)
-            cout<<'>'<<endl;
-        else if(x==y)
-            cout<<'='<<endl;
         else
-            cout<<'<'<<endl;
+        {
+            for(int i=n;i>(n/2)+1;i--)
+                cout<<arr[i]<<" ";
+            for(int i=1;i<=(n/2)+1;i++)
+                cout<<arr[i]<<" ";
+            
+            cout<<endl;
+        }
     }
 
     return 0;
