60ef77655b0b3913b0528177b79cced4d6ec4b8b
mdshakibsami <shakib.sami2.0@gmail.com>
2023-06-28 19:11:18 +0600

    initial commit

--- codeforces/800/TheNewYear_MeetingFriends.cpp
+++ codeforces/800/TheNewYear_MeetingFriends.cpp
@@ -4,9 +4,7 @@ using namespace std;
 int main()
 {
     int arr[3];
-    for (int i = 0; i < 3; i++)
-        cin >> arr[i];
-
+    for (int i = 0; i < 3; i++) cin >> arr[i];
     sort(arr, arr + 3);
     cout << arr[2]-arr[0] << endl;
 }
