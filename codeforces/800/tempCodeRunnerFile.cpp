b81857b562e0fd70ae98cf849fd678ece603a5f6
mdshakibsami <shakib.sami2.0@gmail.com>
2023-07-01 12:18:14 +0600

    initial commit

--- codeforces/800/tempCodeRunnerFile.cpp
+++ codeforces/800/tempCodeRunnerFile.cpp
@@ -1,6 +1,2 @@
-for (int i = 0; i < s.size(); i++)
-                {
-                    if (i == s.size() - 2)
-                        continue;
-                    cout << s[i];
-        
+ // if(i<3) a+=arr[i];
+            // else b+=arr[i];
