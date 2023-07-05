1e0b4c8204441d33a5cffadc1ba4e27178ed5b97
mdshakibsami <shakib.sami2.0@gmail.com>
2023-07-05 23:19:48 +0600

    initial commit

--- codeforces/ProblemSet/800/tempCodeRunnerFile.cpp
+++ codeforces/ProblemSet/800/tempCodeRunnerFile.cpp
@@ -1,10 +1,2 @@
-string s,s2="";
-    cin>>s;
-    int j=0;
-    for(int i=0;i<s.size();i++)
-    {
-        s2[j]=s[i];
-        j++;
-        if(s2[0]==s[j]) break;
-    }
-    for(int i=0;i<s2.size();i++) cout<<s2[i];
+ if(found1 && found2) cout<<"YES"<<endl;
+    
