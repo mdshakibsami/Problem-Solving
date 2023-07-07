a1468e8e7526f47d1989a63865b5ab954715ac50
mdshakibsami <shakib.sami2.0@gmail.com>
2023-07-07 09:15:00 +0600

    initial commit

--- codeforces/ProblemSet/800/sum.cpp
+++ codeforces/ProblemSet/800/sum.cpp
@@ -3,17 +3,25 @@ using namespace std;
 
 int main()
 {
-    int t;
-    cin >> t;
-    while (t--)
+    int n,t;
+    cin>>n>>t;
+    int arr[n];
+    for(int i=0;i<n;i++) cin>>arr[i];
+    
+    int sum=0,count=0;
+    for(int i=0;i<n;i++)
     {
-        int a, b, c;
-        cin >> a >> b >> c;
-        if (a + b == c || b + c == a || a + c == b)
-            cout << "YES" << endl;
-        else
-            cout << "NO" << endl;
+        sum+=arr[i];
+        if(sum<=t)
+        {
+            count++;
+            if(sum==t) break;
+        }
+        else sum-=arr[i];
+        
     }
+    cout<<count<<endl;
+
 
     return 0;
 }
