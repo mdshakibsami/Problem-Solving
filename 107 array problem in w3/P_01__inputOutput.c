7d98352bf486c9fe2d06d5b5b1fdf617d7c8e689
MD Shakib <126575973+mdshakibsami@users.noreply.github.com>
2023-08-01 19:46:08 +0600

    Update P_01__inputOutput.c

--- 107 array problem in w3/P_01__inputOutput.c
+++ 107 array problem in w3/P_01__inputOutput.c
@@ -1,11 +1,13 @@
 #include<stdio.h>
 int main()
 {
-    int i,arr[3];
-    for(int i=0;i<3;i++)
+    int i,arr[4];
+    for(int i=0;i<4;i++)
     {
         scanf("%d",&arr[i]);
         printf("%d ",arr[i]);
     }
+
+
     return 0;
 }
