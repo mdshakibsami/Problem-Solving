5261a40cc90aa5c10535e0440f5bde464d744290
mdshakibsami <shakib.sami2.0@gmail.com>
2023-08-16 20:09:55 +0600

    599A

---
+++ codeforces/ProblemSet/800/Patrick and Shopping/tempCodeRunnerFile.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+    int a,b,c;
+    cin>>a>>b>>c;
+    vector<int>v;
+    int x = a+b+c;
