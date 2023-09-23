2e3dfbd1679da445cc864502e44e421b751d9e4c
mdshakibsami <shakib.sami2.0@gmail.com>
2023-09-23 17:23:24 +0600

    c+=

---
+++ codeforces/ProblemSet/260A.cpp
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int a,b,n;
+    long long x,d;
+    cin>>a>>b>>n;
+    x=a*pow(10,n);
+    cerr<<x<<" ";
+    d =x/b;
+    cerr<<d<<endl;
+    x=(b*d)+b;
+    cout<<x<<" ";                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                          
+    
+}
