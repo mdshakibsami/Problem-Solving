b7ae648748dc1591a23c99d99284910c55ecf402
mdshakibsami <shakib.sami2.0@gmail.com>
2024-03-06 00:17:07 +0600

    Desorting-->800-->A

--- codeforces/ProblemSet/vector.cpp
+++ codeforces/ProblemSet/vector.cpp
@@ -9,7 +9,9 @@ int main()
     for(int i=0;i<10;i++) cin>>v[i];
     for(auto &i:v) cout<<i;//take auto inputs and outputs using address 
 
-
+    vector<int>a,b;
+    a.push_back(10);
+    a.push_back(2);
 
 }
 
