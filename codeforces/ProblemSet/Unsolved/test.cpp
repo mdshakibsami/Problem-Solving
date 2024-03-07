c96e872746a534c0cb170c860387a46618f7f57a
mdshakibsami <shakib.sami2.0@gmail.com>
2024-03-07 22:22:05 +0600

    Make it Beautiful-->800-->A

--- codeforces/ProblemSet/Unsolved/test.cpp
+++ codeforces/ProblemSet/Unsolved/test.cpp
@@ -5,24 +5,8 @@ int main()
     ios_base::sync_with_stdio(false);
     cin.tie(nullptr);
 
-    int t;
-    cin >> t;
-    while (t--)
-    {
-        int n;
-        cin >> n;
-        string s;
-        cin>>s;
-        int ans = n;
-        int left =0,right=n-1;
-
-        while (left<right && s[left]!=s[right])
-        {
-            left++;
-            right--;
-            ans-=2;
-        }
-        cout<<ans<<endl;
-        
-    }
+    int x,y;
+    x=2,y=5;
+    swap(x,y);
+    cout<<x<<y;
 }
